c5847e1e75a30a9edd55f6bc21795901485f797b
Divarci <hasandivarciuk@gmail.com>
2023-04-29 12:31:04 +0100

    Resume Section Completed. Next One is Portfolio Section.

--- CvAndPortfolio/Controllers/HomeController.cs
+++ CvAndPortfolio/Controllers/HomeController.cs
@@ -32,6 +32,25 @@ namespace CvAndPortfolio.Controllers
             var values = db.TBLSKILLs.ToList();
             return PartialView(values);
         }
-        
+        public PartialViewResult Resume()
+        {
+            var values = db.TBLRESUMEs.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult Certificate()
+        {
+            var values = db.TBLCERTIFICATEs.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult Education()
+        {
+            var values = db.TBLEDUCATIONs.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult Experience()
+        {
+            var values = db.TBLEXPERIENCEs.ToList();
+            return PartialView(values);
+        }
     }
 }
