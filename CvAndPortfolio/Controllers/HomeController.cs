0deaaca21583c944016e45a2f41008ac63a18267
Divarci <hasandivarciuk@gmail.com>
2023-04-29 01:21:03 +0100

    Add project files.

---
+++ CvAndPortfolio/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using CvAndPortfolio.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CvAndPortfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        PortfolioAndCvEntities2 db = new PortfolioAndCvEntities2();
+        public ActionResult Index()
+        {
+            var values = db.TBLABOUTs.ToList();
+            return View(values);
+        }
+
+        public PartialViewResult Facts()
+        {
+            var values = db.TBLFACTDESCRIPTIONs.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult PartTwoFacts()
+        {
+            var values = db.TBLFACTs.ToList();
+            return PartialView(values);
+        }
+    }
+}
---
+++ CvAndPortfolio/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CvAndPortfolio.MvcApplication" Language="C#" %>
